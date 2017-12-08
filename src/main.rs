18d6daaf63f82427ad69108ca5cf0c95005889ef
Jake Howard <git@theorangeone.net>
2017-12-08 23:14:32 +0000

    Actually run the formatter

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,8 @@ use rand::Rng;
 
 fn select_file(input_dir: &PathBuf) -> PathBuf {
     let input_files = input_dir.read_dir().expect("Failed to read dir");
-    let mut collected_input_files : Vec<DirEntry> = input_files
-        .map(|d| d.unwrap())
-        .filter(|d| d.path().is_file())
-        .collect();
+    let mut collected_input_files: Vec<DirEntry> =
+        input_files.map(|d| d.unwrap()).filter(|d| d.path().is_file()).collect();
     rand::thread_rng().shuffle(&mut collected_input_files);
     return collected_input_files.pop().expect("Failed to get input file").path();
 }
@@ -25,7 +23,11 @@ fn main() {
     loop {
         let input_file = select_file(&input_dir);
         println!("Setting background to {}...", input_file.display());
-        let output = Command::new("feh").arg("--bg-fill").arg(input_file.display().to_string()).output().expect("Command failed");
+        let output = Command::new("feh")
+            .arg("--bg-fill")
+            .arg(input_file.display().to_string())
+            .output()
+            .expect("Command failed");
         if !output.status.success() {
             eprintln!("{}", String::from_utf8(output.stderr).unwrap());
             eprintln!("Execution failed, Retrying...");
