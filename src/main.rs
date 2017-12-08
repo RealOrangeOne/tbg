99858477d36962e5d2c753d566b3f42b21bef85e
Jake Howard <git@theorangeone.net>
2017-12-08 22:32:46 +0000

    Randomly get files from directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::env;
+use std::path::PathBuf;
+use std::fs::DirEntry;
+use std::thread::sleep;
+use std::time::Duration;
+
+extern crate rand;
+
+use rand::Rng;
+
+fn select_file(input_dir: &PathBuf) -> DirEntry {
+    let input_files = input_dir.read_dir().expect("Failed to read dir");
+    let mut collected_input_files : Vec<DirEntry> = input_files
+        .map(|d| d.unwrap())
+        .filter(|d| d.path().is_file())
+        .collect();
+    rand::thread_rng().shuffle(&mut collected_input_files);
+    return collected_input_files.pop().expect("Failed to get input file");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input_dir = PathBuf::from(env::var("BG_DIR").expect("Missing environment variable BG_DIR"));
+    loop {
+        println!("{:?}", select_file(&input_dir));
+        sleep(Duration::new(10, 0));
+    }
 }
