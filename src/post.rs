0e9a1e61b8dfe3df1d984f61a6057206f25e23f6
Jake Howard <git@theorangeone.net>
2018-01-08 13:37:52 +0000

    Replace symbol in command with background path

--- src/post.rs
+++ src/post.rs
@@ -5,12 +5,15 @@ use std::fs::copy;
 use clap::ArgMatches;
 
 const BG_POINTER_PATH: &str = "/tmp/.wallpaper";
+const CMD_REPLACEMENT: &str = "/_";
 
 pub fn handle_post_image(new_bg: PathBuf, args: &ArgMatches) {
-    copy(new_bg, BG_POINTER_PATH).expect("Failed to copy background pointer");
+    copy(&new_bg, BG_POINTER_PATH).expect("Failed to copy background pointer");
     let cmd_result = args.value_of("command");
     if cmd_result.is_some() {
-        Command::new("bash").arg("-c").arg(cmd_result.unwrap()).output().expect(
+        let cmd = cmd_result.unwrap().replace(CMD_REPLACEMENT, &new_bg.display().to_string());
+        println!("Executing '{}'.", cmd);
+        Command::new("bash").arg("-c").arg(cmd).output().expect(
             "Failed to run post command"
         );
     }
