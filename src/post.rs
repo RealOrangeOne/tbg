14c4e5dadcc688b7b46d0df582e37725a995b5cf
Jake Howard <git@theorangeone.net>
2018-01-08 13:19:58 +0000

    Reformat

--- src/post.rs
+++ src/post.rs
@@ -2,7 +2,6 @@ use std::path::PathBuf;
 use std::process::Command;
 use std::fs::copy;
 
-
 use clap::ArgMatches;
 
 const BG_POINTER_PATH: &str = "/tmp/.wallpaper";
@@ -11,6 +10,8 @@ pub fn handle_post_image(new_bg: PathBuf, args: &ArgMatches) {
     copy(new_bg, BG_POINTER_PATH).expect("Failed to copy background pointer");
     let cmd_result = args.value_of("command");
     if cmd_result.is_some() {
-        Command::new("bash").arg("-c").arg(cmd_result.unwrap()).output().expect("Failed to run post command");
+        Command::new("bash").arg("-c").arg(cmd_result.unwrap()).output().expect(
+            "Failed to run post command"
+        );
     }
 }
